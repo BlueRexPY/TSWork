6432387225b92f2ced59b2aaa9b73e60cbd4dc2a
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-08-28 04:44:26 +0200

    v 0.1.4

--- BACKEND/src/main.ts
+++ BACKEND/src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { env } from 'process';
-import { AppModule } from './app.modules';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const cors = require("cors");
-  const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-  }
-  
-  app.use(cors(corsOptions))
-  await app.listen(process.env.PORT || 5000);
-  console.log(`start server`)
+  await app.listen(3000);
 }
 bootstrap();
